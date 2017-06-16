379da7debc6061e6c32867d59b3b7ed1a80ac4b1
Matt Latham <lath.mj@gmail.com>
2017-06-16 05:13:37 -0700

    Add htmltest to build

---
+++ scripts/htmltest.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Credit to http://pauldambra.github.io/using-travis-to-build-jekyll.html
+
+set -eu
+
+bundle exec htmlproofer \
+  _site \
+  --file-ignore /.git/ \
+  --check-favicon \
+  --check-html \
+  --check-opengraph
